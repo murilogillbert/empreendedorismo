8794750bfbd21eeef1dc96efd4ddb4bff913009f
andreArce <andrearce343@gmail.com>
2026-03-01 19:18:41 -0300

    domain

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(cors({
-    origin: ['https://empreendedorismo-omega.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
+    origin: ['https://www.utable.shop', 'https://utable.shop', 'https://empreendedorismo-omega.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
