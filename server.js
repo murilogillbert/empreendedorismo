e5c78913daa16d85e19587d31e7875bd0843ad31
andreArce <andrearce343@gmail.com>
2026-03-01 14:35:42 -0300

    server.js corrigido

--- server.js
+++ server.js
@@ -1446,4 +1446,7 @@ setInterval(async () => {
 }, 60 * 1000); // Run every 1 minute
 
 const PORT = process.env.PORT || 4242;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
